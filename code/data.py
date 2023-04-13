f9a90376d455b7c20e9ae6eca296f87cf1a88a57
Marvin <289822902@qq.com>
2023-04-14 01:16:54 +0800

    xlnet fix bug

--- code/data.py
+++ code/data.py
@@ -58,7 +58,7 @@ def preprocess_training_examples(examples, tokenizer, max_length = 384, stride =
         context_start = idx
         while sequence_ids[idx] == 1:
             idx += 1
-        context_end = idx - 1
+        context_end = idx - 1 if "xlnet" not in model_checkpoint else idx - 2 
 
         # If the answer is not fully inside the context, label is (0, 0)
         if offset[context_start][0] > start_char or offset[context_end][1] < end_char:
